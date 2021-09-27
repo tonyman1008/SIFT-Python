808b3e5e1ed5e1034b59a0e28eb90602e234b88e
Tony <tonyasdzxc@gmail.com>
2021-09-27 19:27:21 +0800

    add image metics

---
+++ ImageMetrics.py
@@ -0,0 +1,15 @@
+from skimage.metrics import structural_similarity, mean_squared_error, peak_signal_noise_ratio
+import cv2 as cv
+
+img1 = cv.imread("assets/metricsTest/h4.png")
+img2 = cv.imread("assets/metricsTest/h4.png")
+
+ssim = structural_similarity(img1, img2, multichannel=True, gaussian_weights=True,
+                             sigma=1.5, use_sample_covariance=False, data_range=255)
+print("ssim", ssim)
+
+psnr = peak_signal_noise_ratio(img1, img2)
+print("psnr", psnr)
+
+mse = mean_squared_error(img1, img2)
+print("mse", mse)
